ebd57d6ab473c76735e1006b43e94dbb63ea420e
Janne Hellsten <hellsten@live.com>
2020-10-14 14:46:20 +0300

    initial commit

---
+++ harjoitus/src/main/java/com/hellsten/projekti/harjoitus/HarjoitusApplication.java
@@ -0,0 +1,13 @@
+package com.hellsten.projekti.harjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarjoitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarjoitusApplication.class, args);
+	}
+
+}
---
+++ harjoitus/src/test/java/com/hellsten/projekti/harjoitus/HarjoitusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellsten.projekti.harjoitus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarjoitusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
