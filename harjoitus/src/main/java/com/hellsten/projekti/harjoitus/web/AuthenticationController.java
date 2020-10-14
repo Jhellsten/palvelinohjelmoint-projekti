98427cda694c1ab8527a2a4f7d8c2e0ce3caf9fa
Janne Hellsten <hellsten@live.com>
2020-10-15 01:22:31 +0300

    feat: prevent logged in user to get to login page again

--- harjoitus/src/main/java/com/hellsten/projekti/harjoitus/web/AuthenticationController.java
+++ harjoitus/src/main/java/com/hellsten/projekti/harjoitus/web/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.hellsten.projekti.harjoitus.web;
 
-
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AuthenticationController {
 
+    // Checks if current user is logged in
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+          isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
+    }
+
     // Login
     @RequestMapping(value="/login", method = RequestMethod.GET)
         public String login() {
+        if(isAuthenticated() == true) {
+            return "items";
+        }
         return "login";
     }
 
