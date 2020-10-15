413bea241b38e5bfc4acc09c78b5508eaf9d3f91
Janne Hellsten <hellsten@live.com>
2020-10-16 02:16:16 +0300

    fix: added find all method to user repo

--- src/main/java/com/hellsten/projekti/harjoitus/domain/UserRepo.java
+++ src/main/java/com/hellsten/projekti/harjoitus/domain/UserRepo.java
@@ -1,7 +1,10 @@
 package com.hellsten.projekti.harjoitus.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Long> {
     User findByUsername(String username);
+    List<User> findAll();
     }
