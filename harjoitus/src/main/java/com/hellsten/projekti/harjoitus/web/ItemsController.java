21e2a95c936da356bb1c55dc5c54ee30e144d94c
Janne Hellsten <hellsten@live.com>
2020-10-15 19:14:54 +0300

    feat: added signup endpoint, create new item endpoint

--- harjoitus/src/main/java/com/hellsten/projekti/harjoitus/web/ItemsController.java
+++ harjoitus/src/main/java/com/hellsten/projekti/harjoitus/web/ItemsController.java
@@ -2,16 +2,22 @@ package com.hellsten.projekti.harjoitus.web;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.hellsten.projekti.harjoitus.domain.CategoryRepo;
 import com.hellsten.projekti.harjoitus.domain.Item;
 import com.hellsten.projekti.harjoitus.domain.ItemRepo;
+import com.hellsten.projekti.harjoitus.domain.User;
+import com.hellsten.projekti.harjoitus.domain.UserRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,15 +33,17 @@ public class ItemsController {
     private ItemRepo items;
     @Autowired
     private CategoryRepo categories;
+    @Autowired
+    private UserRepo users;
 
     // REST API for all items
-    // @RequestMapping(value="/items", method = RequestMethod.GET)
-    // public @ResponseBody List<Item> itemListRest() {
-    // return (List<Item>) items.findAll();
-    // }
+    @RequestMapping(value="/items", method = RequestMethod.GET)
+    public @ResponseBody List<Item> itemListRest() {
+    return (List<Item>) items.findAll();
+    }
 
     // Get all items
-    @GetMapping("/items")
+    @GetMapping("/index")
     public String getItems(Model model) {
         List<Item> allItems = items.findAll();
         model.addAttribute("items", allItems);
@@ -56,7 +64,7 @@ public class ItemsController {
     @PreAuthorize("hasRole('ADMIN')")
     public String deleteItem(@PathVariable("id") Long itemId, Model model) {
     items.deleteById(itemId);
-    return "redirect:../items";
+    return "redirect:../index";
     }   
 
     // Get values for new item
@@ -115,7 +123,32 @@ public class ItemsController {
     public String saveItem(@ModelAttribute Item item, Model model) {
     System.out.println(item);
     items.save(item);
-    return "redirect:items";
+    return "redirect:index";
+    }   
+
+    // Get signup
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup() {
+    return "signup";
+    }   
+
+    // Save new user
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String saveUser(@Valid User user, BindingResult bindingResults) {
+        System.out.println(users.findByUsername(user.getUsername()));
+        if (bindingResults.hasErrors()) {
+            System.out.println(bindingResults);
+			return "form";
+		}
+
+    if(users.findByUsername(user.getUsername()) == null) {
+        user.setPasswordHash(BCrypt.hashpw(user.getPasswordHash(), BCrypt.gensalt(13)));
+        user.setRole("USER");
+        System.out.println(user);
+        users.save(user);
+    }
+    
+    return "redirect:index";
     }   
 
 }
